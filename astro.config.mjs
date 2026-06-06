df3feeb05fd447b312621be17bf0ae0d05bbbe26
Marco Lanconelli <marko_lan@hotmail.it>
2026-06-06 18:18:27 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mistahuman.github.io',
-  base: 'red-ring',
+  site: 'https://mistahuman.github.io/astro-illustrator-portfolio-starter',
+  base: '/astro-illustrator-portfolio-starter/',
+  output: 'static',
 });
